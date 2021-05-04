811c4ae8bc25b6068fc2af36bb154f131be1e2ed
Lucas Krammes <lucas@krammesnet.com>
2021-05-04 13:24:09 -0400

    Z_STEPPERS_ORIENTATION

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -848,7 +848,7 @@
    *               | 1   2 | 2   3 | 3   4 | 4   1 |
    */
   #ifndef Z_STEPPER_ALIGN_XY
-    //#define Z_STEPPERS_ORIENTATION 0
+    #define Z_STEPPERS_ORIENTATION 0
   #endif
 
   // Provide Z stepper positions for more rapid convergence in bed alignment.
