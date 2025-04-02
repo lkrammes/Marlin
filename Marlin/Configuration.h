2366538f66e04368aee7adcb9cebf33084620fbf
Lucas Krammes <lucas@holeshotsoftware.com>
2025-04-02 11:00:57 -0400

    update bed autotune

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -803,9 +803,9 @@
 
   // 120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   // from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
-  #define DEFAULT_bedKp 227.78
-  #define DEFAULT_bedKi 37.96
-  #define DEFAULT_bedKd 911.11
+  #define DEFAULT_bedKp 236.94
+  #define DEFAULT_bedKi 41.71
+  #define DEFAULT_bedKd 897.21
 
   // FIND YOUR OWN: "M303 E-1 C8 S90" to run autotune on the bed at 90 degreesC for 8 cycles.
 #else
